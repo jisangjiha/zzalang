704578bf2a33cc93091cbc98d81accfec2b446ad
Seokmin <ghdtjrald240@gmail.com>
2024-11-17 18:00:34 +0900

    Add indices for createdAt and authorId

--- api/src/schema.ts
+++ api/src/schema.ts
@@ -1,5 +1,11 @@
 import { sql } from 'drizzle-orm';
-import { blob, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import {
+  blob,
+  index,
+  integer,
+  sqliteTable,
+  text,
+} from 'drizzle-orm/sqlite-core';
 
 const id = integer().primaryKey({ autoIncrement: true });
 
@@ -24,16 +30,23 @@ export const users = sqliteTable('users', {
   passwordHash: blob({ mode: 'buffer' }).notNull(),
 });
 
-export const posts = sqliteTable('posts', {
-  id: id.notNull(),
-  createdAt,
-  updatedAt,
-  title: text().notNull(),
-  content: text().notNull(),
-  authorId: integer()
-    .references(() => users.id)
-    .notNull(),
-});
+export const posts = sqliteTable(
+  'posts',
+  {
+    id: id.notNull(),
+    createdAt,
+    updatedAt,
+    title: text().notNull(),
+    content: text().notNull(),
+    authorId: integer()
+      .references(() => users.id)
+      .notNull(),
+  },
+  (table) => ({
+    createdAtIndex: index('created_at_index').on(table.createdAt),
+    authorIndex: index('author_index').on(table.authorId),
+  }),
+);
 
 export const comments = sqliteTable('comments', {
   id: id.notNull(),
