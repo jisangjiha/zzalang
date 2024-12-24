6ec5f036ba8bc5d975c4ec37df446f855d923be1
Jisang Hong <jisang1024@gmail.com>
2024-12-24 17:26:11 +0900

    logo icon 생성

--- frontend/src/mainpage/Header.tsx
+++ frontend/src/mainpage/Header.tsx
@@ -1,9 +1,11 @@
+import logo from "../assets/casper.svg";
 import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>zzalang</div>
+      <img src={logo} height={30} width={30} />
+      <div className={styles.zzalang}>zzalang</div>
     </header>
   );
 }
