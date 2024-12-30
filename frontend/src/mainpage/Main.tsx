60463dfb81ca57f05dab75857a41795d808e18d5
Jisang Hong <jisang1024@gmail.com>
2024-12-30 16:36:08 +0900

    install react-router-dom

--- frontend/src/mainpage/Main.tsx
+++ frontend/src/mainpage/Main.tsx
@@ -1,8 +1,10 @@
 import styles from "./Main.module.css";
+import { Link } from "react-router-dom";
 
 export default function Main() {
   return (
     <main className={styles.main}>
+      <Link to={`/test`}>test</Link>
       main
       <br />
       d
