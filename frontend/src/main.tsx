f8e53bf81ab9152a6f293d7365458dca4674219a
Jisang Hong <jisang1024@gmail.com>
2024-12-31 18:59:30 +0900

    react-router-dom main 코드 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,27 @@
-//import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import * as React from "react";
-//import * as ReactDOM from "react-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromChildren,
+  Route,
+} from "react-router-dom";
 
 import "./index.css";
 import App from "./App.tsx";
 import Test from "./mainpage/Test.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/test",
-    element: <Test />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <>
+      <Route path="" element={<App />} />
+      <Route path="test" element={<Test />} />
+    </>
+  )
+);
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </StrictMode>
 );
