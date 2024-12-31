da8e304cab64eca867899c3c3ce3e81cba805f5b
Jisang Hong <jisang1024@gmail.com>
2024-12-31 19:10:35 +0900

    header 로그인, 회원가입 css 구성 추가

--- frontend/src/mainpage/Header.tsx
+++ frontend/src/mainpage/Header.tsx
@@ -4,8 +4,14 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img src={logo} height={30} width={30} />
-      <div className={styles.zzalang}>zzalang</div>
+      <div className={styles.logo}>
+        <img src={logo} height={30} width={30} />
+        <div className={styles.zzalang}>zzalang</div>
+      </div>
+      <div className={styles.identify}>
+        <button>회원가입</button>
+        <button>로그인</button>
+      </div>
     </header>
   );
 }
