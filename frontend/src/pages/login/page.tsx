69e94f1455467e19dd4578fa0ab2ec579f09c6d0
Jisang Hong <jisang1024@gmail.com>
2025-01-06 21:19:06 +0900

    로그인 화면 구성

--- frontend/src/pages/login/page.tsx
+++ frontend/src/pages/login/page.tsx
@@ -1,3 +1,15 @@
+import Button from "../../components/Button";
+import InputBox from "../../components/InputBox";
+
+import styles from "../page.module.css";
+
 export default function LoginPage() {
-  return <main>LoginPage</main>;
+  return (
+    <div className={styles.container}>
+      <div>로그인</div>
+      <InputBox placeholder={"@닉네임"} />
+      <InputBox placeholder={"비밀번호"} />
+      <Button buttonName={"로그인"} />
+    </div>
+  );
 }
