cdc63062efa38c956df4ef4eff2aaf540eea10ad
Jisang Hong <jisang1024@gmail.com>
2025-01-14 16:20:18 +0900

    로그인- fetch함수로 API 연결

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,8 @@
+import type { ComponentProps } from "react";
 import styles from "./Button.module.css";
 
-export default function Button(props: { buttonName: string }) {
-  return <button className={styles.button}>{props.buttonName}</button>;
+type ButtonProps = ComponentProps<"button">;
+
+export default function Button({ ...props }: ButtonProps) {
+  return <button {...props} className={styles.button} />;
 }
--- frontend/src/pages/login/page.tsx
+++ frontend/src/pages/login/page.tsx
@@ -9,7 +9,24 @@ export default function LoginPage() {
       <div>로그인</div>
       <InputBox placeholder={"@닉네임"} />
       <InputBox placeholder={"비밀번호"} />
-      <Button buttonName={"로그인"} />
+      <Button
+        onClick={() => {
+          fetch("http://localhost:8787/v1/sign-in", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              handle: "jisang",
+              password: "jisang123",
+            }),
+          }).then(async (res) => {
+            console.log(await res.json());
+          });
+        }}
+      >
+        로그인
+      </Button>
     </div>
   );
 }
