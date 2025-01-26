8b1638a992da8b949e32f771b68272ce783f915a
Jisang Hong <jisang1024@gmail.com>
2025-01-26 19:21:33 +0900

    회원가입- fetxh함수로 API 연결

--- frontend/src/pages/register/page.tsx
+++ frontend/src/pages/register/page.tsx
@@ -1,17 +1,78 @@
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+
+import { AuthContext } from "../../contexts/auth";
 import Button from "../../components/Button";
 import InputBox from "../../components/InputBox";
 
 import styles from "../page.module.css";
 
 export default function RegisterPage() {
+  const navigate = useNavigate();
+  const { setToken } = useContext(AuthContext);
+  const [registerData, setRegisterData] = useState({
+    name: "",
+    handle: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+
   return (
-    <div className={styles.container}>
+    <form
+      className={styles.container}
+      onSubmit={(e) => {
+        e.preventDefault();
+        fetch("http://localhost:8787/v1/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: registerData.name,
+            handle: registerData.handle,
+            password: registerData.password,
+            passwordConfirmation: registerData.passwordConfirmation,
+          }),
+        }).then(async (res) => {
+          const data = await res.json();
+          setToken(data.token);
+          navigate("/");
+        });
+      }}
+    >
       <div>회원가입</div>
-      <InputBox placeholder={"이름"} />
-      <InputBox placeholder={"닉네임"} />
-      <InputBox placeholder={"비밀번호"} />
-      <InputBox placeholder={"비밀번호 확인"} />
-      <Button>회원가입</Button>
-    </div>
+      <InputBox
+        placeholder={"이름"}
+        value={registerData.name}
+        onChange={(e) => {
+          setRegisterData({ ...registerData, name: e.target.value });
+        }}
+      />
+      <InputBox
+        placeholder={"닉네임"}
+        value={registerData.handle}
+        onChange={(e) => {
+          setRegisterData({ ...registerData, handle: e.target.value });
+        }}
+      />
+      <InputBox
+        placeholder={"비밀번호"}
+        value={registerData.password}
+        onChange={(e) => {
+          setRegisterData({ ...registerData, password: e.target.value });
+        }}
+      />
+      <InputBox
+        placeholder={"비밀번호 확인"}
+        value={registerData.passwordConfirmation}
+        onChange={(e) => {
+          setRegisterData({
+            ...registerData,
+            passwordConfirmation: e.target.value,
+          });
+        }}
+      />
+      <Button type="submit">회원가입</Button>
+    </form>
   );
 }
