2404100aa07029236c2c80f0e788111805aaabdb
Jisang Hong <jisang1024@gmail.com>
2025-01-27 22:21:10 +0900

    회원가입- input 조건 검사

--- frontend/src/pages/register/page.tsx
+++ frontend/src/pages/register/page.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useContext, useState } from "react";
 
 import { AuthContext } from "../../contexts/auth";
@@ -16,6 +16,16 @@ export default function RegisterPage() {
     password: "",
     passwordConfirmation: "",
   });
+  //유효성 검사를 위함
+  const [isRegisterData, setIsRegisterDate] = useState({
+    name: false,
+    handle: false,
+    password: false,
+    passwordConfirmation: false,
+  });
+
+  const handleRegExp = /^[a-zA-z0-9]{4,12}$/;
+  const passwordRegExp = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
 
   return (
     <form
@@ -36,42 +46,72 @@ export default function RegisterPage() {
         }).then(async (res) => {
           const data = await res.json();
           setToken(data.token);
-          navigate("/");
+          navigate("/login");
         });
       }}
     >
       <div>회원가입</div>
-      <InputBox
-        placeholder={"이름"}
-        value={registerData.name}
-        onChange={(e) => {
-          setRegisterData({ ...registerData, name: e.target.value });
-        }}
-      />
-      <InputBox
-        placeholder={"닉네임"}
-        value={registerData.handle}
-        onChange={(e) => {
-          setRegisterData({ ...registerData, handle: e.target.value });
-        }}
-      />
-      <InputBox
-        placeholder={"비밀번호"}
-        value={registerData.password}
-        onChange={(e) => {
-          setRegisterData({ ...registerData, password: e.target.value });
-        }}
-      />
-      <InputBox
-        placeholder={"비밀번호 확인"}
-        value={registerData.passwordConfirmation}
-        onChange={(e) => {
-          setRegisterData({
-            ...registerData,
-            passwordConfirmation: e.target.value,
-          });
-        }}
-      />
+      <div className={styles.formBox}>
+        <label>name</label>
+        <InputBox
+          placeholder={"이름"}
+          value={registerData.name}
+          onChange={(e) => {
+            setRegisterData({ ...registerData, name: e.target.value });
+          }}
+        />
+        <p>{registerData.name.length < 2 ? "두 글자 이상 입력하세요" : ""}</p>
+      </div>
+      <div className={styles.formBox}>
+        <label>handle</label>
+        <InputBox
+          placeholder={"닉네임"}
+          value={registerData.handle}
+          onChange={(e) => {
+            setRegisterData({ ...registerData, handle: e.target.value });
+          }}
+        />
+        <p>
+          {!handleRegExp.test(registerData.handle)
+            ? "4-12사이 대소문자 또는 숫자만 입력해 주세요"
+            : ""}
+        </p>
+      </div>
+      <div className={styles.formBox}>
+        <label>password</label>
+        <InputBox
+          placeholder={"비밀번호"}
+          value={registerData.password}
+          onChange={(e) => {
+            setRegisterData({ ...registerData, password: e.target.value });
+          }}
+        />
+        <p>
+          {registerData.handle === registerData.password
+            ? "handle과 같을 수 없습니다"
+            : !passwordRegExp.test(registerData.password)
+            ? "숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요"
+            : ""}
+        </p>
+      </div>
+      <div className={styles.formBox}>
+        <label>password confirmation</label>
+        <InputBox
+          placeholder={"비밀번호 확인"}
+          value={registerData.passwordConfirmation}
+          onChange={(e) => {
+            setRegisterData({
+              ...registerData,
+              passwordConfirmation: e.target.value,
+            });
+          }}
+        />
+        <p>
+          {registerData.password !== registerData.passwordConfirmation
+            ? "비밀번호가 일치하지 않습니다"
+            : ""}
+        </p>
+      </div>
       <Button type="submit">회원가입</Button>
     </form>
   );
