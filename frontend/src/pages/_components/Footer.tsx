096cdae62b1b82d7c53456d93db6e6a2ec1caf3c
Jisang Hong <jisang1024@gmail.com>
2025-03-21 22:43:33 +0900

    copyright 현재 시간 기준으로 수정

--- frontend/src/pages/_components/Footer.tsx
+++ frontend/src/pages/_components/Footer.tsx
@@ -1,5 +1,11 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}>ⓒ 2025. Jisang Hong</footer>;
+  const today = new Date();
+
+  return (
+    <footer className={styles.footer}>
+      ⓒ {today.getFullYear()}. Jisang Hong
+    </footer>
+  );
 }
