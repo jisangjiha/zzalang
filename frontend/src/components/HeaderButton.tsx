eac178efcba9435ea51300b6de012c6e5ecaf26b
Jisang Hong <jisang1024@gmail.com>
2025-03-27 22:06:26 +0900

    마이페이지 버튼 생성

--- frontend/src/components/HeaderButton.tsx
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps } from "react";
-import styles from "./HeaderButton.module.css";
-
-type ButtonProps = ComponentProps<"button">;
-
-export default function HeaderButton({ ...props }: ButtonProps) {
-  return <button {...props} className={styles.button} />;
-}
