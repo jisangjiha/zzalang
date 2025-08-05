52d8c0435b3f45898ae55f2f782f7d4e11e4b539
Jisang Hong <jisang1024@gmail.com>
2025-08-05 23:41:23 +0900

    fix: isLoginPage 조건 처리 Record 타입으로 변경

--- frontend/src/components/AuthRouter.tsx
+++ frontend/src/components/AuthRouter.tsx
@@ -6,16 +6,30 @@ type AuthRouterProps = {
   className?: string;
 };
 
+const LINK_PROPS: Record<
+  AuthRouterProps["type"],
+  { question: string; linkText: string; linkTo: string }
+> = {
+  login: {
+    question: "계정이 없으신가요?",
+    linkText: "회원가입하러 가기",
+    linkTo: "/register",
+  },
+  register: {
+    question: "이미 계정이 있으신가요?",
+    linkText: "로그인하러 가기",
+    linkTo: "/login",
+  },
+} as const;
+
 export default function AuthRouter({ type, className }: AuthRouterProps) {
-  const isLoginPage = type === "login";
+  const props = LINK_PROPS[type];
 
   return (
     <div className={`${styles.container} ${className || ""}`}>
-      <span className={styles.text}>
-        {isLoginPage ? "계정이 없으신가요?" : "이미 계정이 있으신가요?"}
-      </span>
-      <Link to={isLoginPage ? "/register" : "/login"} className={styles.link}>
-        {isLoginPage ? "회원가입하러 가기" : "로그인하러 가기"}
+      <span className={styles.text}>{props.question}</span>
+      <Link to={props.linkTo} className={styles.link}>
+        {props.linkText}
       </Link>
     </div>
   );
