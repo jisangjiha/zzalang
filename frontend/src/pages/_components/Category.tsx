4bd6f1eeacfb0031aef1e0149c2b48c18d91d01b
Jisang Hong <jisang1024@gmail.com>
2025-12-09 23:06:57 +0900

    feat: 게시판 카테고리 분리로 의한 계획

--- frontend/src/pages/_components/Category.tsx
+++ frontend/src/pages/_components/Category.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./Category.module.css";
 
+// 게시글 작성할 때 카테고리 선택할 수 있도록 해야 함
+// 백엔드 필요
+
+// 메인페이지 구성 어떻게 할 건지 기획 후 페이지 route 분리
+
 export default function Category() {
   return (
     <div className={styles.category}>
